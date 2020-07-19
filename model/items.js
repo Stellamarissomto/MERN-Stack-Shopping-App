011e2e4f50c62a7f4e4cf063e52731873ee18dc5
Stellamarissomto <stellamarissomto@gmail.com>
2020-07-19 11:04:31 +0100

    added model

---
+++ model/items.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ItemSchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  });
+  
+module.exports = Item = mongoose.model('item', ItemSchema);
