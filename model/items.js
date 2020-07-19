e024a4e9723ca5dd230e6b20233147d2c7fd96eb
Stellamarissomto <stellamarissomto@gmail.com>
2020-07-19 12:02:15 +0100

    added route

--- model/items.js
+++ model/items.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const ItemSchema = new Schema({
+const ItemSchema = new mongoose.Schema ({
     name: {
       type: String,
       required: true
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
+const items = require("./route/api/items");
 
 
 // load dotenv file
@@ -12,6 +13,7 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/api/v1/items', items);
 
 // connection to db
 connectDB();
